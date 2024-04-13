8e044b5b341faa09d4a8f3ab91996385a246c0c9
Mojtaba Shirkhani <mojtabashirkhani.nba@gmail.com>
2024-04-13 13:04:07 +0330

    set pagesize to 10, remove paging util that not used

--- app/src/main/java/com/example/weldy/fragment/catList/CatListFragmentVM.kt
+++ app/src/main/java/com/example/weldy/fragment/catList/CatListFragmentVM.kt
@@ -6,22 +6,17 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.example.weldy.api.Resource
-import com.example.weldy.api.WeldyApi
 import com.example.weldy.data.CatResponse
 import com.example.weldy.repo.CatRepository
 import com.example.weldy.repo.paged.CatSource
-import com.example.weldy.util.GeneralPagingSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flattenMerge
-import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 @HiltViewModel
 class CatListFragmentVM @Inject constructor(private val catRepository: CatRepository): ViewModel() {
 
-    val user: Flow<PagingData<CatResponse>> = Pager(PagingConfig(pageSize = 100)) {
+    val user: Flow<PagingData<CatResponse>> = Pager(PagingConfig(pageSize = 10)) {
         CatSource(catRepository)
     }.flow.cachedIn(viewModelScope)
 }
